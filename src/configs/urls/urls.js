cae745c63bf926a972606542fec216c17b6fdae8
unknown <gamergodakshay@gmail.com>
2023-08-17 11:32:57 +0530

    adding url

--- src/configs/urls/urls.js
+++ src/configs/urls/urls.js
@@ -1,5 +1,5 @@
 export const __ENV = "prod";
-let URL = "https://156.67.218.228/api"
+let URL = "https://dev-finscre.online/api"
 
 if(__ENV == "dev"){
     URL = "http://localhost:4001/api"
@@ -7,5 +7,5 @@ if(__ENV == "dev"){
 
 export const SOCKET_URL ="http://localhost:4000";
 export const BASE_URL = URL ;
-export const UPLOAD_URL="https://156.67.218.228/upload";
-export const PAYMENT_URL="https://156.67.218.228/api/pay";
+export const UPLOAD_URL="https://dev-finscre.online/upload";
+export const PAYMENT_URL="https://dev-finscre.online/api/pay";
